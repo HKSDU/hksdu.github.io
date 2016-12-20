c8a08ff2193780f390982626b007cc1f99d667b8
wingswork <wingtk@gmail.com>
2016-12-20 10:55:38 +0800

    Update 20161220.js

--- login/20161220.js
+++ login/20161220.js
@@ -3,7 +3,7 @@ var attempt = 3; // Variable to count number of attempts.
 function validate(){
 var password = document.getElementById("password").value;
 if ( password == "ak47"){
-window.location = "index.html"; // Redirecting to other page.
+window.location = "\ak47\index.html"; // Redirecting to other page.
 return false;
 }
 else{
